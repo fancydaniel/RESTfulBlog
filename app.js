a13a9a80a21642ce4df65e1ac1af3755404bc153
Daniel Uribe <duribe8@gmail.com>
2019-07-04 17:53:41 -0700

    Added Mongoose Model & Schema

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ mongoose = require("mongoose"),
 express = require("express"),
 app = express();
 
+// App Config
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+
+//RESTful Routes
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("Server is running!")
